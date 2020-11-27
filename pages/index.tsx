67283ab049d384043ff604aeabb045202c3323a9
upatisariputa <upatisariputa@gmail.com>
2020-11-27 16:37:11 +0900

    Make serverside rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { END } from "redux-saga";
 import AppLayout from "../components/AppLayout";
 import { RootState } from "../reducers";
 import PostForm from "../components/PostForm";
 import PostCard from "../components/PostCard";
 import { LOAD_POSTS_REQUEST } from "../reducers/post";
 import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
+import wrapper from "../store/configureStore";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -18,15 +20,6 @@ const Home = () => {
       alert(retweetError);
     }
   }, [retweetError]);
-
-  useEffect(() => {
-    dispatch({
-      type: LOAD_MY_INFO_REQUEST,
-    });
-    dispatch({
-      type: LOAD_POSTS_REQUEST,
-    });
-  }, []);
 
   useEffect(() => {
     function onScroll() {
@@ -56,5 +49,17 @@ const Home = () => {
     </AppLayout>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  console.log("실행되니??");
+  context.store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  context.store.dispatch({
+    type: LOAD_POSTS_REQUEST,
+  });
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+});
 
 export default Home;
--- reducers/index.ts
+++ reducers/index.ts
@@ -5,19 +5,32 @@ import user from "./user";
 import post from "./post";
 
 // (이전상태, 액션) => 다음상태
-const rootReducer = combineReducers({
-  index: (state: object = {}, action) => {
-    switch (action.type) {
-      case HYDRATE:
-        console.log("HYDRATE", action);
-        return { ...state, ...action.payload };
-      default:
-        return state;
+// const rootReducer = combineReducers({
+//   index: (state: object = {}, action) => {
+//     switch (action.type) {
+//       case HYDRATE:
+//         console.log("HYDRATE", action);
+//         return { ...state, ...action.payload };
+//       default:
+//         return state;
+//     }
+//   },
+//   user,
+//   post,
+// });
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case HYDRATE:
+      console.log("하이드레이트", action);
+      return { ...state, ...action.payload };
+    default: {
+      const combineReducer = combineReducers({ user, post });
+      return combineReducer(state, action);
     }
-  },
-  user,
-  post,
-});
+  }
+};
+
 export default rootReducer;
 
 export type RootState = ReturnType<typeof rootReducer>;
