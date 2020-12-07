f35d8c3b9b6dbc69e8ec5c58a04161d34e0a3fb3
upatisariputa <upatisariputa@gmail.com>
2020-12-07 11:06:02 +0900

    Modify server url

--- config/config.ts
+++ config/config.ts
@@ -1,1 +1,1 @@
-export const backURL = process.env.NODE_ENV === "production" ? "http://3.35.205.47" : "http://localhost:3065";
+export const backURL = process.env.NODE_ENV === "production" ? "http://52.79.222.60" : "http://localhost:3065";
